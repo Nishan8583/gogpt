4e88021d84f2a1c07023c8fc8df3c2ba0c9ffd47
nishan8583 <nishanmaharjan17@gmail.com>
2023-12-29 08:31:29 -0800

    more error

--- openAI/scan_code.go
+++ openAI/scan_code.go
@@ -11,7 +11,7 @@ var errEmtpyResonse = errors.New("empty response from OpenAI")
 
 // ScanCode adds prompt indicating that its a cyber security researcher
 func (oa OpenAI) ScanCode(code string) (string, error) {
-	msg := fmt.Sprintf(`Could you find a vulnerability in the following code %s`, code)
+	msg := fmt.Sprintf(`Find a vulnerability in the following code %s`, code)
 	resp, err := oa.Chat([]Message{
 		{Role: "system", Content: "you are a cyber security researcher who has experience doing source code audit, code review and in finding vulnerabilities in source code in multiple different programming languages."},
 		{Role: "user", Content: msg},
